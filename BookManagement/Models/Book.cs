ead3148eff2084b4feca93c32e312d00500bd7b9
GithsaraS <Ishancharun@icloud.com>
2024-12-29 03:03:25 +0530

    Add project files.

---
+++ BookManagement/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
+{
+    public class Book
+    {
+        [Key]
+        public string id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string isbn { get; set; }
+        public string publicationDate { get; set; }
+        
+    }
+}
---
+++ BookManagement/Models/BookDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public object Book { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost; initial Catalog=book; User Id=sa; password=Ishan#5411696; TrustServerCertificate=True");
+        }
+    }
+}
